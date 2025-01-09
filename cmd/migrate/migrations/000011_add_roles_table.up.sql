0e2f845f855dfa5f0f855db43b39aa01a19d8830
Ian Gechuki <iangechuki@gmail.com>
2025-01-09 11:43:58 +0300

    authorization database migrations

---
+++ cmd/migrate/migrations/000011_add_roles_table.up.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS roles(
+    id BIGSERIAL PRIMARY KEY,
+    name varchar(255) UNIQUE NOT NULL,
+    level int NOT NULL DEFAULT 0,
+    description TEXT
+);
+INSERT INTO 
+    roles (name, level, description) 
+VALUES
+    ('user',
+     1,
+     'A user can create posts and comments'
+     );
+
+INSERT INTO
+    roles (name, level, description)
+VALUES
+    ('moderator',
+     2,
+     'A moderator can update other users posts'
+     );
+    
+INSERT INTO
+    roles (name, level, description)
+VALUES
+    ('admin',
+     3,
+     'An admin can update and delete other users posts'
+     );
